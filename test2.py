02e3470c6b6bffb732ccf8b3686beeb71697531a
Lidia Trzepaczka <l.trzepaczka@gmail.com>
2020-03-17 20:12:54 +0100

    Selenium Easy - Exercises "Radio Button Demo" and "Group Radio Buttons Demo"

--- test2.py
+++ test2.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.select import Select
 from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(executable_path=r'C:\Users\Lydyjka\.wdm\drivers\chromedriver\80.0.3987.106\win32\chromedriver.exe')
 #driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html")
-driver.get("https://www.seleniumeasy.com/test/basic-checkbox-demo.html")
+driver.get("https://www.seleniumeasy.com/test/basic-radiobutton-demo.html")
 driver.maximize_window()
 
 #cwiczenie: Single Input Field
@@ -31,9 +31,24 @@ driver.maximize_window()
 #driver.find_element_by_id("printAll").click()  #probably there is a bug because the second element is displaying
 
 #cwiczenie: Checkbox Demo
-driver.find_element_by_id("isAgeSelected").click()
+#driver.find_element_by_id("isAgeSelected").click()
 
 #cwiczenie:Multiple Chexbox Demo
-driver.find_element_by_xpath("//label[text()='Option 1']//input[@type='checkbox']").click()
-driver.find_element_by_xpath("//label[text()='Option 2']//input[@type='checkbox']").click()
-driver.find_element_by_id("check1").click()
+#driver.find_element_by_xpath("//label[text()='Option 1']//input[@type='checkbox']").click()
+#driver.find_element_by_xpath("//label[text()='Option 2']//input[@type='checkbox']").click()
+#driver.find_element_by_id("check1").click()
+
+#cwiczenie: Radio Button Demo
+driver.find_element_by_xpath("//*[@name='optradio' and @value='Male']").click()
+driver.find_element_by_id("buttoncheck").click()
+driver.find_element_by_xpath("//*[@name='optradio' and @value='Female']").click()
+driver.find_element_by_id("buttoncheck").click()
+
+#cwiczenie: Group Radio Buttons Demo
+driver.find_element_by_xpath("//*[@name='gender' and @value='Male']").click()
+driver.find_element_by_xpath("//*[text()='5 to 15']//input[@type='radio']").click()
+driver.find_element_by_xpath("//button[text()='Get values']").click()
+
+driver.find_element_by_xpath("//*[@name='gender' and @value='Female']").click()
+driver.find_element_by_xpath("//*[text()='0 to 5']//input[@type='radio']").click()
+driver.find_element_by_xpath("//button[text()='Get values']").click()
