19b3ede6e884e6de6f809587742b09b7852483a6
Lidia Trzepaczka <l.trzepaczka@gmail.com>
2020-03-17 19:43:46 +0100

    Selenium Easy - Exercises "Single Checkbox Demo" and "Multiple Checkbox Demo"

--- test2.py
+++ test2.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.select import Select
 from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(executable_path=r'C:\Users\Lydyjka\.wdm\drivers\chromedriver\80.0.3987.106\win32\chromedriver.exe')
 #driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html")
-driver.get("https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html")
+driver.get("https://www.seleniumeasy.com/test/basic-checkbox-demo.html")
 driver.maximize_window()
 
 #cwiczenie: Single Input Field
@@ -18,14 +18,22 @@ driver.maximize_window()
 #print(driver.find_element_by_id("displayvalue").text)
 
 #cwiczenie: Select List Demo
-auto_select = Select(driver.find_element_by_id("select-demo"))
-auto_select.select_by_visible_text("Monday")
-print(driver.find_element_by_class_name("selected-value").text)
+#auto_select = Select(driver.find_element_by_id("select-demo"))
+#auto_select.select_by_visible_text("Monday")
+#print(driver.find_element_by_class_name("selected-value").text)
 
 #cwiczenie: Multi Select List Demo
-auto_select2 = Select(driver.find_element_by_id("multi-select"))
-auto_select2.select_by_value("California")
-auto_select2.select_by_value("Ohio")
-auto_select2.select_by_value("Florida")
-driver.find_element_by_id("printMe").click()
-driver.find_element_by_id("printAll").click()  #probably there is a bug because the second element is displaying
+#auto_select2 = Select(driver.find_element_by_id("multi-select"))
+#auto_select2.select_by_value("California")
+#auto_select2.select_by_value("Ohio")
+#auto_select2.select_by_value("Florida")
+#driver.find_element_by_id("printMe").click()
+#driver.find_element_by_id("printAll").click()  #probably there is a bug because the second element is displaying
+
+#cwiczenie: Checkbox Demo
+driver.find_element_by_id("isAgeSelected").click()
+
+#cwiczenie:Multiple Chexbox Demo
+driver.find_element_by_xpath("//label[text()='Option 1']//input[@type='checkbox']").click()
+driver.find_element_by_xpath("//label[text()='Option 2']//input[@type='checkbox']").click()
+driver.find_element_by_id("check1").click()
