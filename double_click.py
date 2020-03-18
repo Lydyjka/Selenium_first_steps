d4987b3ad2fb5d3526afa172989b2d78401b9935
Lidia Trzepaczka <l.trzepaczka@gmail.com>
2020-03-18 18:52:44 +0100

    Test: Double click simulation

---
+++ double_click.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(executable_path=r'C:\Users\Lydyjka\.wdm\drivers\chromedriver\80.0.3987.106\win32\chromedriver.exe')
+
+driver.get("file:///C:/Users/Lydyjka/Desktop/selenium_kurs_udemy/DoubleClick.html")
+
+button = driver.find_element_by_id("bottom")
+
+webdriver.ActionChains(driver).double_click(button).perform()
