b76ae7c9d570dcccaa8ae5ea41a714f0014fa08c
Lidia Trzepaczka <l.trzepaczka@gmail.com>
2020-03-18 18:51:16 +0100

    Test: Clear field input

--- test1.py
+++ test1.py
@@ -64,3 +64,7 @@ driver.get("file:///C:/Users/Lydyjka/Desktop/selenium_kurs_udemy/Test.html")
 # print(driver.title)
 # driver.switch_to.window(current_window_name)
 # print(driver.title)
+
+username_input = driver.find_element_by_name("username")
+username_input.clear()
+username_input.send_keys("Lydyjka")
