33ade6d49e8afbb09a26c905b6c3fe87a4fb7df8
Lidia Trzepaczka <l.trzepaczka@gmail.com>
2020-03-18 20:54:19 +0100

    Test: upload .txt file

---
+++ upload_file.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import os
+
+driver = webdriver.Chrome(executable_path=r'C:\Users\Lydyjka\.wdm\drivers\chromedriver\80.0.3987.106\win32\chromedriver.exe')
+
+driver.get("file:///C:/Users/Lydyjka/Desktop/selenium_kurs_udemy/FileUpload.html")
+
+upload_input = driver.find_element_by_id("myFile")
+path = os.path.abspath("upload.txt")
+
+upload_input.send_keys(path)
