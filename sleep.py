7bc41b4a7ccc20cad2b0479438ef421d86a8a427
Lidia Trzepaczka <l.trzepaczka@gmail.com>
2020-03-20 15:42:38 +0100

    Test: implicity_wait method

--- sleep.py
+++ sleep.py
@@ -3,8 +3,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 driver = webdriver.Chrome(executable_path=r'C:\Users\Lydyjka\.wdm\drivers\chromedriver\80.0.3987.106\win32\chromedriver.exe')
 
-driver.get("file:///C:/Users/Lydyjka/Desktop/selenium_kurs_udemy/Waits.html")
-
+driver.implicitly_wait(10)
+driver.get("file:///C:/Users/Lydyjka/Desktop/selenium_kurs_udemy/Waits2.html")
 driver.find_element_by_id("clickOnMe").click()
-time.sleep(5)
+# time.sleep(5)
 print(driver.find_element_by_tag_name("p").text)
