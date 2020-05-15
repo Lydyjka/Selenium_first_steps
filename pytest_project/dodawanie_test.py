373c4c3ded39c45b3ebef1fd550e92e227364461
Lidia Trzepaczka <l.trzepaczka@gmail.com>
2020-05-15 17:43:23 +0200

    Using pytest.mark.parametrize decorator

---
+++ pytest_project/dodawanie_test.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+@pytest.mark.parametrize("skladnik,suma", [(5, 8), (2, 4)])
+def test_dodawania(skladnik, suma):
+    assert skladnik + skladnik == suma, "Suma dwoch takich samych skladnikow powinna byc rowna: " + str(skladnik + skladnik)
