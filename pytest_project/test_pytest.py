39048f6c6c2fc77d5ff489c2021872597841bdfb
Lidia Trzepaczka <l.trzepaczka@gmail.com>
2020-05-15 17:47:51 +0200

    Marking tests as failed or skipped

--- pytest_project/test_pytest.py
+++ pytest_project/test_pytest.py
@@ -1,6 +1,20 @@
 import pytest
 
+@pytest.mark.skip
 def test_method():
+    x = 5
+    y = 2
+    assert x + y == 7, "Assertion failed, Expected 7"
+    assert x + y == 2, "Assertion failed, Expected 2"
+
+def test_method2():
+    x = 5
+    y = 2
+    assert x + y == 7, "Assertion failed, Expected 7"
+    assert x + y == 7, "Assertion failed, Expected 7"
+
+@pytest.mark.xfail
+def test_method3():
     x = 5
     y = 2
     assert x + y == 7, "Assertion failed, Expected 7"
