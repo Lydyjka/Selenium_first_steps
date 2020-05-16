6a6d60e6273c28e6831eb90856f160280a24e9b9
Lidia Trzepaczka <l.trzepaczka@gmail.com>
2020-05-16 22:06:13 +0200

    Testing web-application functionalities: assertions for search results

--- web_app_testing/search_hotel_test.py
+++ web_app_testing/search_hotel_test.py
@@ -24,3 +24,25 @@ driver.find_element_by_id("adultInput").send_keys("4")
 driver.find_element_by_id("childInput").clear()
 driver.find_element_by_id("childInput").send_keys("4")
 driver.find_element_by_xpath("//button[text()=' Search']").click()
+
+hotels = driver.find_elements_by_xpath("//h4[contains(@class,'list_title')]//b")
+hotel_names = [hotel.get_attribute("textContent") for hotel in hotels]
+for name in hotel_names:
+    print("Hotel name: " + name)
+
+prices = driver.find_elements_by_xpath("//div[contains(@class,'price_tab')]//b")
+price_values = [price.get_attribute("textContent") for price in prices]
+for price in price_values:
+    print("Cena to: " + price)
+
+assert hotel_names[0] == 'Jumeirah Beach Hotel'
+assert hotel_names[1] == 'Oasis Beach Tower'
+assert hotel_names[2] == 'Rose Rayhaan Rotana'
+assert hotel_names[3] == 'Hyatt Regency Perth'
+
+assert price_values[0] == '$22'
+assert price_values[1] == '$50'
+assert price_values[2] == '$80'
+assert price_values[3] == '$150'
+
+driver.quit()
